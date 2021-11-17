874e2705942fd900559367cffbf124e37b7d0295
Sabbir-Rahman <sabbirrahman42@iut-dhaka.edu>
2021-11-18 02:21:10 +0600

    profile page ongoing

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import Topbar from "./components/topbar/Topbar"
 import Home from './pages/home/Home'
+import Profile from "./pages/home/profile/Profile";
 
 function App() {
   return (
     <>
-      <Topbar/>
-      <Home/>
+      <Profile/>
     </>
   );
 }

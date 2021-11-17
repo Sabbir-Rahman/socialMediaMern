33c0574d4d731b666e096a494202afa24725a8d1
Sabbir-Rahman <sabbirrahman42@iut-dhaka.edu>
2021-11-18 01:20:12 +0600

    like dislike functionality

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./post.css"
 import {MoreVert} from "@material-ui/icons"
 import { Users } from '../../dummyData'
 
 export default function Post({post}) {
-    console.log(Users.filter((u) => u.id === 1)[0].username)
-    if(post){
-        
+    const [like,setLike] = useState(post?.like)
+    const [isLiked,setIsLiked] = useState(false)
+
+    const likeHandler = () => {
+        setLike(isLiked? like-1: like+1)
+        setIsLiked(!isLiked)
     }
+
     return (
         <div className="post">
             <div className="postWrapper">
@@ -34,9 +38,9 @@ export default function Post({post}) {
                 </div>
                 <div className="postBottom">
                     <div className="postBottomLeft">
-                        <img className="likeIcon" src="assets/like.png" alt=""/>
+                        <img className="likeIcon" src="assets/like.png" onClick={likeHandler} alt=""/>
                         <img className="likeIcon" src="assets/heart.png" alt=""/>
-                        <span className="postLikeCounter">{post?.like} people like it</span>
+                        <span className="postLikeCounter">{like} people like it</span>
                     </div>
                     <div className="postBottomRight">
                         <span className="postCommentText">{post?.comment} comments</span>

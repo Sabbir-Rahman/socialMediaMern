bdac729ae99c33581bcf11ead186482e089e7143
Sabbir-Rahman <sabbirrahman42@iut-dhaka.edu>
2021-11-19 01:29:23 +0600

    register page done

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
-import Login from "./components/login/Login";
+import Login from "./pages/login/Login";
 import Topbar from "./components/topbar/Topbar"
 import Home from './pages/home/Home'
-import Profile from "./pages/home/profile/Profile";
+import Profile from "./pages/profile/Profile";
+import Register from "./pages/register/Register"
 
 function App() {
   return (
     <>
-      <Login/>
+    
+      <Register/>
     </>
   );
 }

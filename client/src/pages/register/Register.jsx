fdad5ed689a737fff98f65b96b34e8fe280b93a7
Sabbir-Rahman <sabbirrahman42@iut-dhaka.edu>
2021-12-12 14:49:48 +0600

    register and login tested

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,7 +1,38 @@
+import { useRef } from "react"
 import React from 'react'
 import "./register.css"
+import axios from "axios"
+import { useHistory } from "react-router"
 
 export default function Register() {
+    const username = useRef()
+    const email = useRef()
+    const password = useRef()
+    const passwordAgain = useRef()
+    const history = useHistory()
+
+    const handleClick = async (e) => {
+        
+        e.preventDefault()
+        if(passwordAgain.current.value !== password.current.value){
+            passwordAgain.current.setCustomValidity("Password don't match")
+        }else {
+            const user = {
+                username: username.current.value,
+                email: email.current.value,
+                password: password.current.value
+            }
+
+            try {
+                await axios.post("http://localhost:8800/api/v1/auth/register",user)
+                history.push("/login")
+            }catch(err){
+                console.log(err)
+            }
+            
+        }
+    }
+
     return (
         <div className="register">
             <div className="registerWrapper">
@@ -12,16 +43,16 @@ export default function Register() {
                     </span>
                 </div>
                 <div className="registerRight">
-                    <div className="registerBox">
-                        <input placeholder="Username" className="registerInput"/>
-                        <input placeholder="Email" className="registerInput"/>
-                        <input placeholder="Password" className="registerInput"/>
-                        <input placeholder="Password Again" className="registerInput"/>
-                        <button className="registerButton">Sign Up</button>
+                    <form className="registerBox" onSubmit={handleClick}>
+                        <input placeholder="Username" required ref={username} className="registerInput" type="text"/>
+                        <input placeholder="Email" required ref={email} className="registerInput" type="email"/>
+                        <input placeholder="Password" required ref={password} className="registerInput" type="password" minLength="6"/>
+                        <input placeholder="Password Again" required ref={passwordAgain} className="registerInput" type="password" minLength="6"/>
+                        <button className="registerButton" type="submit">Sign Up</button>
                         <button className="registerRegisterButton">
                             Log into account
                         </button>
-                    </div>
+                    </form>
                 </div>
             </div>
         </div>
